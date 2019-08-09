fa2cf9149a60c2ad20bf9dbd8b84f5325d926944
bmcculley <box76135@gmail.com>
2019-08-08 23:09:20 -0400

    add contact form

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+	$('.alert').removeClass('hide').hide();
+    $('#send-btn').on('click', function(e){
+    	console.log('contact form submitted.');
+    	var datastring = $('#contact-form').serialize();
+    	$.ajax({
+            type: 'POST',
+            url: 'https://ebg.xyz/contact.php',
+            data: datastring,
+            success: function(data) {
+            	$('#myModal').modal('hide');
+                $('.alert').slideDown('slow');
+                $('#msg').text('Thank you for your message, I\'ll repsond shortly.');
+                setTimeout(function(){ 
+                	$('.alert').slideUp('slow');
+                }, 3000);
+            }
+        });
+    })
+});
